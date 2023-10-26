6b70da5bd29640b6f61199d46207da24cc421b70
Nicolas Santi <nicolas.santi@outlook.com.ar>
2023-10-25 22:27:23 -0300

    agrego componente Main con categorías

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import './main.css'
+
+function Main() {
+  return (
+    <main className='main'>
+        <ul className='mainUl'>
+            <li className='mainUlLi'><a href='/Mates'>Mates</a></li>
+            <li className='mainUlLi'><a href='/Termos'>Termos</a></li>
+            <li className='mainUlLi'><a href='/Bombillas'>Bombillas</a></li>
+            <li className='mainUlLi'><a href='/Accesorios'>Accesorios</a></li>
+        </ul>
+    </main>
+
+  )
+}
+
+export default Main
